e9848753b936a344cb3b3cad216c793c12eed9b6
fatakeshto <fatakeshto@example.com>
2025-02-28 08:09:15 +0600

    oauth2_scheme

--- backend/utils.py
+++ backend/utils.py
@@ -5,13 +5,15 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from database import get_db
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = os.environ.get("SECRET_KEY", secrets.token_hex(32))
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 def get_password_hash(password):
     return pwd_context.hash(password)
