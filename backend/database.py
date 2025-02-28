9a701aa4fe2c0ab6d8d2965f84d3037b6f58d7d4
fatakeshto <fatakeshto@example.com>
2025-02-28 07:53:16 +0600

    fixing the SQLAlchemy

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,8 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+Base = declarative_base()
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql+asyncpg://neondb_owner:npg_efE9hVpwUM5n@ep-patient-truth-a15jezae-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require")
 
